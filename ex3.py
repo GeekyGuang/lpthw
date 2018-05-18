a657ae61034322c4f3551953499fd465016433eb
TravisWongX <travis.x.wong@gmail.com>
2018-05-18 10:43:01 +0800

    adding three exercises

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print("I will now check my chickens:") # print a promt
+
+print("Hens", 25.0 + 30.0 / 6.0) # print the amount of hens
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0) # print the amount of Roosters
+
+print("Now I will count the eggs:") # print a prompt
+
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0) # calculating the eggs
+
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?") # print a prompt
+
+print(3.0 + 2.0 < 5.0 - 7.0) # make sure if 3 + 2 less than 5 - 7
+
+print("What is 3.0 + 2.0?", 3.0 + 2.0) # calculating 3 + 2
+print("What is 5.0 - 7.0?", 5.0 - 7.0) # calculating 5 - 7
+
+print("Oh, that's why it's False.") # print a conclusion
+
+print("How about some more?") # print a prompt
+
+print("Is it greater?", 5.0 > -2.0) # make sure if 5 greater than -2
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
