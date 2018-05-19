51696c2571b49767118dd66785445a28bc798c8e
TravisWongX <travis.x.wong@gmail.com>
2018-05-19 21:07:51 +0800

    2018-05-19

---
+++ ex8.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Marry me Juliet never have to be alone,",
+    "I love you and that's all I really know,",
+    "I talked to your dad go pick out a white dress,",
+    "It's a love story baby just say yes."
+))
