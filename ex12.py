d7162333e6a5ead85ceb971daefdd853a68066fa
TravisWongX <travis.x.wong@gmail.com>
2018-05-20 10:19:49 +0800

    2018-05-20

---
+++ ex12.py
@@ -0,0 +1,6 @@
+age = int(input("How old are you? ")) # combine what you've learned
+height = input(f"You are {age}? How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you are {age} old, {height} tall and {weight} heavy.")
+print(f'{"Nice!"}') # you can't use double-quotes or single-quotes twice
